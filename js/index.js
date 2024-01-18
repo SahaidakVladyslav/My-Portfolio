0fc2ad971f4eb84f07b3283b31875b4fdeb79fc9
Vladyslav Sahaidak <svg.231@gmail.com>
2024-01-18 14:29:45 +0100

    'addJavaFormSent'

--- js/index.js
+++ js/index.js
@@ -1,11 +1,96 @@
 
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import throttle from 'lodash.throttle';
 
 
-const bodyEl = document.querySelector('body')
-console.log()
+const keyLocalStorage = "feedback-form-state";
+
+const formEl = document.querySelector('form');
+const textareaEl = document.querySelector('textarea');
+const inputsEl = {
+    name: Array.from(document.querySelectorAll('.backdrop__input'))[0],
+    telephone: Array.from(document.querySelectorAll('.backdrop__input'))[1],
+    email: Array.from(document.querySelectorAll('.backdrop__input'))[2],
+};
+const chekInputEl = document.querySelector('#signature')
+
+let data = {
+    name: '',
+    telephone: '',
+    email: '',
+    message: ''
+};
+
+const storageSet = () => {
+    data.name = inputsEl.name.value
+    data.telephone = inputsEl.telephone.value
+    data.email = inputsEl.email.value
+    data.message = textareaEl.value
+    localStorage.setItem(keyLocalStorage, JSON.stringify(data))
+}
+
+const storageGet = () => {
+
+    if (localStorage.getItem(keyLocalStorage)) {
+        const parseData = JSON.parse(localStorage.getItem(keyLocalStorage))
+        inputsEl.name.value = parseData.name
+        inputsEl.telephone.value = parseData.telephone
+        inputsEl.email.value = parseData.email
+        textareaEl.value = parseData.message
+    }
+}
+
+iziToast.settings({
+    timeout: 10000,
+    resetOnHover: true,
+    icon: 'material-icons',
+    transitionIn: 'flipInX',
+    transitionOut: 'flipOutX',
+    position: 'topRight',
+    onOpening: function () {
+        console.log('callback abriu!');
+    },
+    onClosing: function () {
+        console.log("callback fechou!");
+    }
+});
 
 
-bodyEl.addEventListener('click', throttle(function sayMamba(event) {
-    console.log('mamba')
+storageGet()
+
+localStorage.removeItem(keyLocalStorage)
+
+formEl.addEventListener('input', throttle(function sayMamba(event) {
+    if (event.target === textareaEl || Object.values(inputsEl).includes(event.target)) {
+        storageSet()
+    } else {
+        return;
+    }
 }, 500));
+
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+
+    if ((!inputsEl.name.value || !inputsEl.telephone.value || !inputsEl.email.value || !textareaEl.value)) {
+        return iziToast.error({
+            title: 'Error',
+            message: 'Please fill in the blank input fields',
+        });
+    }
+    if (!chekInputEl.checked) {
+        return iziToast.info({
+            title: 'Hello',
+            message: 'Please agree to the privacy policy!',
+        });
+    }
+    iziToast.success({
+        title: 'Success',
+        message: 'Data sent successfully!',
+    });
+    localStorage.removeItem(keyLocalStorage)
+    console.log(data)
+    formEl.reset()
+})
